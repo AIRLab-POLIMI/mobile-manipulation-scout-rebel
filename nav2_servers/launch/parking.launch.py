b82284201deba7c4e5a80b6aeac9daedc29752bb
SimonGiampy <simonegiampa99@gmail.com>
2024-02-24 18:05:11 +0100

    moved parking demo and action servers from scout_nav2 repository

---
+++ nav2_servers/launch/parking.launch.py
@@ -0,0 +1,16 @@
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # start the demo autonomy task
+    navigate_demo_node = Node(
+        name="parking_demo",
+        package="nav2_servers",
+        executable="parking_demo.py",
+        emulate_tty=True,
+        output="screen",
+    )
+
+    return LaunchDescription([navigate_demo_node])
