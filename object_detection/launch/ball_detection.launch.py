e1abd0b33330f1a0830800f32ffcbbfdd30b91a9
SimonGiampy <simonegiampa99@gmail.com>
2024-04-23 14:18:33 +0200

    ball object detection inference with tensorflow in ros2 node

---
+++ object_detection/launch/ball_detection.launch.py
@@ -0,0 +1,174 @@
+
+# ROS2 imports
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration, TextSubstitution
+from launch.actions import IncludeLaunchDescription, TimerAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch_ros.substitutions import FindPackageShare
+from launch.conditions import IfCondition, UnlessCondition
+
+# Python imports
+import os
+from ament_index_python.packages import get_package_share_directory
+import yaml
+
+# load moveit controllers and parameters from yaml and robot description files
+from moveit_launch import moveit_loader
+
+
+def generate_launch_description():
+
+    # Load camera parameters from the config file
+    camera_params_file = os.path.join(
+        get_package_share_directory('soft_grasping'),
+        'config',
+        'params.yaml'
+    )
+
+    with open(camera_params_file, 'r') as file:
+        config = yaml.safe_load(file)
+
+    config = config["/**"]["ros__parameters"]
+
+    rgb_topic_arg = DeclareLaunchArgument(
+        name='rgb_topic',
+        default_value=config['rgb_topic'],
+        description='Name of the image RGB topic provided by the camera node',
+    )
+
+    depth_image_topic_arg = DeclareLaunchArgument(
+        name='depth_topic',
+        default_value=config['depth_topic'],
+        description='Name of the depth image topic provided by the camera node',
+    )
+
+    pointcloud_topic_arg = DeclareLaunchArgument(
+        name='pointcloud_topic',
+        default_value=config['pointcloud_topic'],
+        description='Name of the pointcloud topic provided by the camera node',
+    )
+
+    camera_info_topic_arg = DeclareLaunchArgument(
+        name='camera_info_topic',
+        default_value=config['camera_info_topic'],
+        description='Name of the camera info topic provided by the camera node',
+    )
+
+    camera_rgb_frame_arg = DeclareLaunchArgument(
+        name='camera_rgb_frame',
+        default_value=config['camera_rgb_frame'],
+        description='Camera frame of reference for rgb images',
+    )
+
+    camera_depth_frame_arg = DeclareLaunchArgument(
+        name='camera_depth_frame',
+        default_value=config['camera_depth_frame'],
+        description='Camera frame of reference for depth images',
+    )
+
+    # declare launch arguments from moveit loader
+    args = moveit_loader.declare_arguments()
+    # read camera frame from aruco_pose_estimation config file
+    camera_frame_arg = moveit_loader.load_camera_frame_arg()
+
+    args.append(camera_frame_arg)
+
+    # launch realsense camera node
+    cam_feed_launch_file = PathJoinSubstitution(
+        [FindPackageShare("realsense2_camera"), "launch", "rs_launch.py"]
+    )
+
+    camera_feed_depth_node = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(cam_feed_launch_file),
+        launch_arguments={
+            "pointcloud.enable": "true",
+            "enable_rgbd": "true",
+            "enable_sync": "true",
+            "align_depth.enable": "true",
+            "enable_color": "true",
+            "enable_depth": "true",
+        }.items(),
+    )
+
+    # launch target click node: click on the target on the RGB image window to get the pixel coordinates
+    # of the target in the camera frame, for testing purposes
+    ball_detector_node = Node(
+        name="ball_detector",
+        package='object_detection',
+        executable='ball_detector.py',
+        parameters=[{
+            'rgb_topic': LaunchConfiguration('rgb_topic'),
+        }],
+        output='screen',
+        emulate_tty=True
+    )
+
+    # delay start of the grasping pose estimator node until the realsense camera node is up and running
+    ball_detector_node_delayed = TimerAction(
+        period=1.0,
+        actions=[
+            LaunchDescription([
+                ball_detector_node
+            ])
+        ]
+    )
+
+    # launch grasping pose estimator node
+    grasping_pose_estimator_node = Node(
+        name='grasp_pose_estimator',
+        package='soft_grasping',
+        executable='grasp_pose_estimator',
+        parameters=moveit_loader.load_moveit(with_sensors3d=False) + [{
+            # parameters for moveit2_apis library
+            "camera_frame": LaunchConfiguration('camera_rgb_frame'),
+            "load_base": LaunchConfiguration("load_base"),
+            # parameters for the grasping pose estimator node
+            'pointcloud_topic': LaunchConfiguration('pointcloud_topic'),
+            'depth_topic': LaunchConfiguration('depth_topic'),
+            'camera_info_topic': LaunchConfiguration('camera_info_topic'),
+            'camera_rgb_frame': LaunchConfiguration('camera_rgb_frame'),
+            'camera_depth_frame': LaunchConfiguration('camera_depth_frame'),
+        }],
+        output='screen',
+        emulate_tty=True
+    )
+
+    # delay start of the grasping pose estimator node until the realsense camera node is up and running
+    grasping_pose_estimator_node_delayed = TimerAction(
+        period=1.0,
+        actions=[
+            LaunchDescription([
+                grasping_pose_estimator_node
+            ])
+        ]
+    )
+
+    # launch rviz2 with the target click rviz configuration file
+    rviz_file = PathJoinSubstitution([
+        FindPackageShare('soft_grasping'),
+        'rviz',
+        'target_click.rviz'
+    ])
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', rviz_file]
+    )
+
+    return LaunchDescription(args + [
+        # Arguments
+        rgb_topic_arg,
+        pointcloud_topic_arg,
+        depth_image_topic_arg,
+        camera_info_topic_arg,
+        camera_rgb_frame_arg,
+        camera_depth_frame_arg,
+        # Nodes
+        camera_feed_depth_node,
+        ball_detector_node_delayed,
+        grasping_pose_estimator_node_delayed,
+        rviz2_node
+    ])
---
+++ object_detection/scripts/ball_detector.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+"""
+Author: Simone Giampà
+University: Politecnico di Milano
+Master Degree: Computer Science Engineering
+Laboratory: Artificial Intelligence and Robotics Laboratory (AIRLab)
+
+"""
+
+# ROS2 imports
+import rclpy
+from rclpy.qos import qos_profile_sensor_data
+from rclpy.node import Node
+from rcl_interfaces.msg import ParameterDescriptor, ParameterType
+
+# messages imports
+from geometry_msgs.msg import PoseStamped
+from sensor_msgs.msg import Image
+
+# custom ROS2 interfaces
+from mobile_manipulation_interfaces.msg import ObjectDetections
+
+# OpenCV python imports
+import cv2
+from cv_bridge import CvBridge
+
+# Tensorflow imports
+import tensorflow as tf
+import keras_cv
+
+# python imports
+import threading
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+from ament_index_python.packages import get_package_share_directory
+
+
+class BallDetector(Node):
+
+    TEXT_COLOR = (255, 255, 255)  # White
+    COLOR_MAPPING = {
+        0: (0, 0, 255),  # blue
+        1: (0, 255, 0),  # green
+        2: (255, 0, 0),  # red
+        3: (255, 250, 0),  # yellow
+    }
+
+    def __init__(self):
+        super().__init__('ball_detector_node')
+
+        self.initialize_parameters()
+
+        # create subscription to /camera/color/image_raw topic
+        self.rgb_sub = self.create_subscription(Image, self.rgb_topic, self.rgb_image_callback, 10)
+        self.rgb_sub  # prevent unused variable warning
+
+        # refresh image in cv2 window at 30Hz within a timer callback
+        self.timer_period = 1.0 / 30.0  # 30Hz [ms]
+        self.timer = self.create_timer(self.timer_period, self.timer_callback)
+
+        # create publisher of target pose coordinates
+        self.detections_pub = self.create_publisher(ObjectDetections, '/object_detections', 10)
+
+        self.load_model_inference()
+
+        # openCV for image visualization
+        self.bridge = CvBridge()
+        self.window_name = "RGB Feed"
+        cv2.namedWindow(self.window_name)
+        self.cv_image = None
+
+        # create continuously running thread for performing inference with the trained model
+        self.inference_thread = threading.Thread(target=self.inference_thread)
+        self.inference_thread.start()
+
+    def rgb_image_callback(self, img_msg: Image):
+        """Subscriber callback to RGB image
+
+        Parameters:
+        ----------
+        msg : Image
+            the RGB image message received from the topic
+
+        """
+        # do something with the image
+        self.cv_image = self.bridge.imgmsg_to_cv2(img_msg, desired_encoding="bgr8")
+
+    def load_model_inference(self):
+        # class mapping = dictionary from class index to class name
+        self.class_mapping = {
+            0: "blue_ball",
+            1: "green_ball",
+            2: "red_ball",
+            3: "yellow_ball"
+        }
+
+        # print the tensorflow version and the available GPU devices
+        self.get_logger().info("Tensorflow version: ", tf.__version__)
+        self.get_logger().info("GPU is", "available" if tf.config.list_physical_devices('GPU') else "NOT AVAILABLE")
+
+        # load the trained model for inference
+        self.model_path = os.path.join(
+            get_package_share_directory("object_detection"),
+            "models",
+            "yolov8s_100.keras"
+        )
+        self.yolo_model = tf.keras.models.load_model(self.model_path)
+        self.get_logger().info("Model loaded successfully!")
+        self.yolo_model.compile(
+            classification_loss="binary_crossentropy",  # multi-hot encoded labels require this loss function
+            box_loss="ciou",  # complete intersection over union
+            # disable just in time compilation with XLA for compatibility issues using GPU acceleration during training,
+            # due to CombinedNonMaxSuppression op not currently supported by tensorflow
+            jit_compile=False
+        )
+        self.get_logger().info("Model compiled successfully!")
+
+    def inference_thread(self):
+        rate = self.create_rate(1)
+        while (rclpy.ok()):
+            if self.cv_image is not None:
+                # perform inference with the trained model
+                # and publish the detected object coordinates
+
+                img = np.expand_dims(self.cv_image, axis=0)
+                self.get_logger().info(f"Performing inference...")
+
+                # measure the inference time
+                start_time = cv2.getTickCount()
+
+                # perform inference with the trained model
+                predictions = self.yolo_model.predict(img)
+
+                # measure the inference time
+                end_time = cv2.getTickCount()
+                inference_time = (end_time - start_time) / cv2.getTickFrequency()
+                self.get_logger().info(f"Inference time: {inference_time:.3f} s")
+
+                # visualize the detected object predictions
+                bboxes = np.array(predictions["boxes"][0])
+                labels = np.array(predictions["classes"][0])
+                confidence = np.array(predictions["confidence"][0])
+                self.cv_predicted = self.visualize_single_pred(img, bboxes, labels, confidence)
+
+                # publish the detected object coordinates
+                object_detections = ObjectDetections()
+                object_detections.header.stamp = self.get_clock().now().to_msg()
+                object_detections.header.frame_id = "camera_link"
+                # linearize the bounding boxes vector
+                # and store the detected object coordinates with their respective class labels
+                object_detections.bboxes = bboxes.flatten().tolist()
+                object_detections.labels = labels.tolist()
+                object_detections.confidences = confidence.tolist()
+
+                # publish the detected object coordinates
+                self.detections_pub.publish(object_detections)
+
+            else:
+                # sleep until a new image is received
+                self.get_logger().info("Waiting for a new image...")
+                rate.sleep()
+
+    def visualize_single_pred(self, image, bboxes, labels, confidences):
+        """Visualizes a batch of images with bounding boxes in a single plot."""
+        batched_image = image.numpy().astype(np.int32).copy()
+        image = np.squeeze(batched_image, axis=0)
+        for bbox, label, confidence in zip(bboxes, labels, confidences):
+            if int(label) != -1:
+                class_name = self.class_mapping[label]
+                image = self.visualize_bbox(
+                    image,
+                    bbox,
+                    class_name,
+                    confidence=f"{confidence:.2f}",
+                    color=self.COLOR_MAPPING[label],
+                )
+        return image
+
+    def visualize_bbox(self, img, bbox, class_name, color, confidence=""):
+        """Visualizes a single bounding box on the image"""
+        x_min, y_min, w, h = bbox
+        x_min, x_max, y_min, y_max = int(x_min), int(x_min + w), int(y_min), int(y_min + h)
+
+        # display rectangle with class name and confidence
+        cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=2)
+        display_text = (class_name + " | " + confidence) if confidence != "" else class_name
+        ((text_width, text_height), _) = cv2.getTextSize(display_text, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)
+        cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), color, -1)
+        cv2.putText(
+            img,
+            text=display_text,
+            org=(x_min, y_min - int(0.3 * text_height)),
+            fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+            fontScale=0.35,
+            color=self.TEXT_COLOR,
+            lineType=cv2.LINE_AA,
+        )
+        return img
+
+    def timer_callback(self):
+        """Timer callback
+
+        Callback function for timer event
+        """
+
+        # self.get_logger().info(f"displaying image... {self.cv_image is not None}")
+        if self.cv_predicted is not None:
+            # display processed image with detected object predictions
+            cv2.imshow(self.window_name, self.cv_predicted)
+            cv2.waitKey(1)
+        elif self.cv_image is not None:
+            # display raw image
+            cv2.imshow(self.window_name, self.cv_image)
+            cv2.waitKey(1)
+
+    def initialize_parameters(self):
+        # Declare and read parameters from aruco_params.yaml
+        self.declare_parameter(
+            name="rgb_topic",
+            value="/camera/color/image_raw",
+            descriptor=ParameterDescriptor(
+                type=ParameterType.PARAMETER_STRING,
+                description="RGB image topic to subscribe to",
+            ),
+        )
+
+        # read parameters from config/params.yaml and store them
+        self.rgb_topic = (
+            self.get_parameter("rgb_topic").get_parameter_value().string_value
+        )
+        self.get_logger().info(f"RGB image topic: {self.rgb_topic}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = BallDetector()
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
